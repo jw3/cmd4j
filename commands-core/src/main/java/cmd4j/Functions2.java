da00be4b53b37e54f384bf25ab7a736ba9e21ae9
John Wass <wassj@ctc.com>
2014-02-04 12:46:26 -0500

    aggregating functions

--- commands-core/src/main/java/cmd4j/Functions2.java
+++ commands-core/src/main/java/cmd4j/Functions2.java
@@ -1,9 +1,16 @@
 package cmd4j;
 
+import java.util.Collection;
+import java.util.Map;
+
 import cmd4j.ICommand.ICommand4;
 import cmd4j.ICommand.IFunction;
 
 import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 
 /**
  *
@@ -39,6 +46,54 @@ public class Functions2 {
 				catch (final Exception e) {
 					throw new RuntimeException("command invocation failed", e);
 				}
+			}
+		};
+	}
+
+
+	/**
+	 * transform the specified {@link IFunction} into a collection handling function
+	 * @param function
+	 * @return
+	 */
+	public static <I, O> IFunction<Collection<I>, Collection<O>> collection(final IFunction<I, O> function) {
+		return new IFunction<Collection<I>, Collection<O>>() {
+			public Collection<O> invoke(final Collection<I> inputs) {
+				return Collections2.transform(inputs, function(function));
+			}
+		};
+	}
+
+
+	/**
+	 * transform the specified {@link IFunction} into a collection handling function
+	 * the output will be a {@link Map} containing the transformeds mapped to the original
+	 * @param function
+	 * @return
+	 */
+	public static <I, O> IFunction<Collection<I>, Map<I, O>> map(final IFunction<I, O> function) {
+		return new IFunction<Collection<I>, Map<I, O>>() {
+			public Map<I, O> invoke(final Collection<I> inputs) {
+				return Maps.toMap(inputs, function(function));
+			}
+		};
+	}
+
+
+	/**
+	 * transform the specified {@link IFunction} into a collection handling function
+	 * the output will be a {@link Multimap} containing the transformeds mapped to the original
+	 * @param function
+	 * @return
+	 */
+	public static <I, O> IFunction<Collection<I>, Multimap<I, O>> multimap(final IFunction<I, Collection<O>> function) {
+		return new IFunction<Collection<I>, Multimap<I, O>>() {
+			public Multimap<I, O> invoke(final Collection<I> inputs) {
+				final Multimap<I, O> result = HashMultimap.create();
+				for (final I input : inputs) {
+					result.putAll(input, function(function).apply(input));
+				}
+				return result;
 			}
 		};
 	}
