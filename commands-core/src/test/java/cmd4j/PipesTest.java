17495f7d2f9857e6b845d0bdb7c7d737f71c9e7f
John Wass <wassj@ctc.com>
2014-12-08 08:08:40 -0500

    additional pipe test

--- commands-core/src/test/java/cmd4j/PipesTest.java
+++ commands-core/src/test/java/cmd4j/PipesTest.java
@@ -19,6 +19,8 @@ public class PipesTest {
 	@Mocked
 	private ICommand2<Integer> _2a;
 	@Mocked
+	private ICommand2<Integer> _2b;
+	@Mocked
 	private ICommand3<Integer> _3a;
 	@Mocked
 	private ICommand4<Integer, Integer> _4a;
@@ -125,5 +127,45 @@ public class PipesTest {
 			.build()
 			.invoke(1);
 		Assert.assertEquals(result, 9);
+	}
+
+
+	/**
+	 * Illustrates that a pipe will overwrite the input value for the remainder of the execution scope, not just for the next command
+	 * 
+	 * test a chain that 
+	 * 1) receives an input value
+	 * 2) executes a command with that value
+	 * 3) #2 command pipes out its result
+	 * 4) #3 command receives #2 output
+	 * 5) #4 command revceive #2 output
+	 * 
+	 */
+	@Test
+	public void pipeOverwritesInput()
+		throws Exception {
+
+		final Integer original = 1;
+		final Integer replaced = 2;
+
+		new Expectations() {
+			{
+				_4a.invoke(original);
+				times = 1;
+				result = replaced;
+				_2a.invoke(replaced);
+				times = 1;
+				_2b.invoke(replaced);
+				times = 1;
+			}
+		};
+
+		Chains.builder() //
+			.add(_4a)
+			.add(Commands.pipe())
+			.add(_2a)
+			.add(_2b)
+			.build()
+			.invoke(original);
 	}
 }
